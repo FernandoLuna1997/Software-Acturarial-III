1a3f3810642acb228b9f16cf63bf7d111874ebf2
Fernando Luna <fer2_life@hotmail.com>
2018-05-21 23:50:37 -0500

    act

--- Caso2/rankingcompleto.R
+++ Caso2/rankingcompleto.R
@@ -2,47 +2,46 @@ setwd("C:/Users/fer2_/Documents/GitHub/Hospitales")
 
 rankingcompleto <- function(resultado, num = "mejor") {
     
-    data <- read.csv("outcome-of-care-measures.csv")
+    outcome <- read.csv("outcome-of-care-measures.csv")
     
     if (resultado == "ataque"){
-    }else if (resultado == "falla") {
-    }else if (resultado == "neumonia"){
-    }else { stop("Resultado Invalido")
-        break
+            }else if (resultado == "falla") {
+                }else if (resultado == "neumonia"){
+                    }else { stop("Resultado Invalido")
+                            break
     }
     
-    col <- if (resultado == "ataque") {
+    r <- if (resultado == "ataque") {
         11 
-    } else if (resultado == "falla") {
-        17 
-    } else {
-        23 
+            } else if (resultado == "falla") {
+                17 
+                    } else {
+                        23 
     }
-    data[, col] <- suppressWarnings(as.numeric(levels(data[, col])[data[, col]]))
-    data[, 2] <- as.character(data[, 2])
-    resultadoFin <- vector()
-    estados <- levels(data[, 7])
+    outcome[, r] <- suppressWarnings(as.numeric(levels(outcome[, r])[outcome[, r]]))
+    outcome[, 2] <- as.character(outcome[, 2])
+    Fin <- vector()
+    estados <- levels(outcome[, 7])
     for(i in 1:length(estados)) {
-        DataEstado <- data[grep(estados[i], data$State), ]
-        Ordenar_Data <- DataEstado[order(DataEstado[, col], DataEstado[, 2], na.last = NA), ]
-        
-        hospital <- if(num == "mejor") {
-            Ordenar_Data[1, 2]
+        Tablaes <- outcome[grep(estados[i], outcome$State), ]
+        Ord <- Tablaes[order(Tablaes[, r], Tablaes[, 2], na.last = NA), ]
+        hos <- if(num == "mejor") {
+            Ord[1, 2]
         } else if(num == "peor") {
-            Ordenar_Data[nrow(Ordenar_Data), 2]
+            Ord[nrow(Ord), 2]
         } else{
-            Ordenar_Data[num, 2]
+            Ord[num, 2]
         }
         
-        resultadoFin <- append(resultadoFin, c(hospital, estados[i])) 
+        Fin <- append(Fin, c(hos, estados[i])) 
     }
     
-    resultadoFin <- as.data.frame(matrix(resultadoFin, length(estados), 2, byrow = TRUE))
-    colnames(resultadoFin) <- c("hospital", "estado")
-    rownames(resultadoFin) <- estados
-    resultadoFin
+    Fin <- as.data.frame(matrix(Fin, length(estados), 2, byrow = TRUE))
+    colnames(Fin) <- c("hospital", "estado")
+    rownames(Fin) <- estados
+    Fin
     
 }
 
-head(rankingcompleto("ataque",20),10)
-tail(rankingcompleto("neumonia","peor"),3)
+head(rankingcompleto("ataque",20),5)
+tail(rankingcompleto("neumonia","peor"),5)
